ef4b3b473a54dfe92cbb28640b87ff2339820c78
will <procterw@gmail.com>
2019-05-04 13:41:32 -0700

    Refactors app to use vue pt.1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="example">{{ msg }}</div>
+</template>
+
+<script>
+const routerStore = {
+  debug: true,
+  state: {
+    route: '#'
+  },
+}
+
+export default {
+  data () {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<style>
+.example {
+  color: red;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
